363274baf37abb820d4d782e01956ada09ee86e8
thiagojordao98 <thiagojordao98@gmail.com>
2022-10-16 16:57:30 -0300

    feat: django-CRUD

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome    
